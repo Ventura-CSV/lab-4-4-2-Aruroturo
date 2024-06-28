7346504d57c2c233969e6eed1f5cbc1304621bf8
Aruroturo <arturo.alfaro3@my.vcccd.edu>
2024-06-27 20:41:50 -0700

    peep

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 def main():
+    while True:
+        try:
+            Person = int(input("Number: "))
+            number = float(Person)
+        except ValueError:
+            print("Input must be numeric")
+        else:
+            print(number)
+            break
     """
     ########################################
     Code Your Program here
     ########################################
     """
-
+    
+    
     ########################################
     # Do not delete the return statement
     ########################################
